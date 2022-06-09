2e6964a1e30f8c65639d59f713847ea3bfdccf88
pebore <peboreayobami@gmail.com>
2022-06-09 09:31:49 -0700

    junioh i the building

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main- check the code
+ *
+ * return always 0
+ **/
+int _isupper(int c)
+{
+	char c;
+
+	if(c>='A' &&  c<='Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+
+#ifndef Main_h
+#define Main_h
+int _isupper(int c)
+
+#endif
